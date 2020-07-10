d90185f042fcd7967d0443ea2a30e4dec7f4faa5
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-10 20:57:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+
+
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker (name)
+ "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (array)
+  new_array=[]
+  array.each do |name|
+    new_array.push("Hello, my name is #{name}.")
+  end
+  return new_array
+end
+ 
+def assign_rooms(array)
+  room_number=1
+  array.each_with_index do |name|
+    puts ""
